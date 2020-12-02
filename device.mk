17e77fc2eb4d7ddc45fa8465a166e6741b027011
Ayush <ayush@disroot.org>
2020-12-02 19:57:53 +0530

    CPH1859: remove Ims boot jars
    * also remove non needed files for ims (pre-included in Ims apk)
    
    Signed-off-by: Ayush <ayush@disroot.org>

--- device.mk
+++ device.mk
@@ -156,16 +156,6 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 PRODUCT_TARGET_VNDK_VERSION := 28
 PRODUCT_EXTRA_VNDK_VERSIONS := 28
 
-# Ims Jars
-PRODUCT_BOOT_JARS += \
-    mediatek-common \
-    mediatek-framework \
-    mediatek-ims-base \
-    mediatek-ims-common \
-    mediatek-telecom-common \
-    mediatek-telephony-base \
-    mediatek-telephony-common
-
 PRODUCT_PACKAGES += \
     RcsService
 
